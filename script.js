140246fbdf291f7a6e9701f16ed130c40f90e5e7
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-09-09 11:21:35 -0300

    html e vetores feitos

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
+let nomes = []
+let preços = []
+let quantidades = []
+let valortotal = []
+
+
+
+
+
+
+
+
+
+
+
 function Cadastro() {
-    document.getElementById("corpo").classList.add('hidden')
-    document.getElementById("Adicionar").classList.remove('hidden')    
+
+  
+
+
+document.getElementById("teste").classList.remove("hidden")
+//document.getElementById("tabelaa").classList.add("hidden")
+document.getElementById("testando").classList.add("hidden")
+
+
+
+
+}
+
+
+
+function Salvar() {
+
+    let nome = document.getElementById("nome")
+
+    let preco = document.getElementById("preco")
+    
+    let quantidade = document.getElementById("quantidade")
+    
+
+
+    nomes.push(nome.value)
+    preco.push(parseFloat(preco.value))
+    
+    quantidade.push(parseFloat(quantidade.value))
+    
+    
+    let total = parseFloat(preco.value) * parseFloat(quantidade.value)
+    valortotal.push(total)
+    
+    alert("Cadastro realizado com sucesso!")
+}
+
+function Cancelar (){
 
 }
