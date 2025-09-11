50ab9529df7eb613e5c5793dfd834dbcecd87c99
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-09-11 10:47:48 -0300

    js acabado(botões e a tabela)  e iniciando css

--- script.js
+++ script.js
@@ -1,56 +1,77 @@
 let nomes = []
-let preços = []
+let precos = []
 let quantidades = []
 let valortotal = []
 
 
-
-
-
-
-
-
-
-
-
 function Cadastro() {
 
-  
 
+    document.getElementById("tabela").classList.add("hidden")
+    document.getElementById("Cadastro").classList.remove("hidden")
 
-document.getElementById("teste").classList.remove("hidden")
-//document.getElementById("tabelaa").classList.add("hidden")
-document.getElementById("testando").classList.add("hidden")
 
 
 
 
 }
-
-
 
 function Salvar() {
 
     let nome = document.getElementById("nome")
 
     let preco = document.getElementById("preco")
-    
+
     let quantidade = document.getElementById("quantidade")
-    
+
+   
+
 
 
     nomes.push(nome.value)
-    preco.push(parseFloat(preco.value))
-    
-    quantidade.push(parseFloat(quantidade.value))
-    
-    
-    let total = parseFloat(preco.value) * parseFloat(quantidade.value)
+    precos.push(parseFloat(preco.value))
+
+    quantidades.push(parseFloat(quantidade.value))
+
+
+    let total = parseFloat(preco.value) * parseInt(quantidade.value)
     valortotal.push(total)
-    
+
     alert("Cadastro realizado com sucesso!")
+    mostrarTabela()
 }
 
-function Cancelar (){
+function mostrarTabela() {
+    let corpotabela = document.getElementById("corpo");
+    corpotabela.innerHTML = ""
+    for (let i = 0; i < nomes.length; i++) {
+        const linha = document.createElement("tr")
+        linha.innerHTML = `
+                <th>${nomes[i]}</th>
+                <th>${precos[i]}</th>
+                <th>${quantidades[i]}</th>
+                <th>${valortotal[i]}</th> 
+               <th> <button onclick="excluir(${i})">excluir</button></th> 
+                `
+        corpotabela.appendChild(linha)
+
+        document.getElementById("tabela").classList.remove("hidden")
+        document.getElementById("Cadastro").classList.add("hidden")
+    }
+}
+function excluir(i){
+
+   
+    nomes.splice(i,1)
+    precos.splice(i,1)
+    quantidades.splice(i,1)
+    valortotal.splice(i,1)
+
+    mostrarTabela()
+}
+
+
+
+function Cancelar() {
 
 }
