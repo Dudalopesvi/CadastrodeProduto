efc5aec7c8caeac01e98a2a5e3f268a26fb4ee4e
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-09-18 10:56:37 -0300

    Finalizando

---
+++ objetos3.js
@@ -0,0 +1,43 @@
+
+const prompt = require("prompt-sync")()
+
+let alugueis = []
+
+let quantidademaxima = parseInt(prompt("quantos quartos você quer alugar?"))
+
+for (let i = 0; i < quantidademaxima; i++) {
+
+    let quarto = prompt("Qual quarto você escolheu?")
+    if (ocupado(quarto)) {
+        console.log("Quarto ocupado tente outro")
+        i--
+    } else {
+        let nome = prompt("Qual seu nome?")
+        let email = prompt("Qual o seu email?")
+
+        let aluguel = {
+            nome: nome,
+            email: email,
+            quarto: quarto,
+        }
+    }
+
+
+
+    aluguel.push(alugueis)
+
+
+}
+
+console.log("Quartos ocupados")
+
+function ocupado(quarto) {
+
+    for (let i = 0; i < alugueis.length; i++) {
+        if (alugueis[i].quarto == quarto) {
+            return true
+        }
+
+    }
+    return false
+}
